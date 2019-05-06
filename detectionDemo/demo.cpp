979b3dbf38e7f138aa0019c420b65164390ecbae
heiheiya <122619445@qq.com>
2019-05-06 11:00:23 +0800

    add class manager

--- detectionDemo/demo.cpp
+++ detectionDemo/demo.cpp
@@ -4,13 +4,14 @@
 #include <chrono>
 #include <opencv2/opencv.hpp>
 #include "../src/detection_class.hpp"
-//#include "../src/AlgoDetection.h"
+#include "../src/detection.h"
+#include "../src/AlgoDetectionManager.h"
 
 #pragma comment(lib, "AlgoDetection.lib")
 #pragma comment(lib, "opencv_world320.lib")
 
 
-void show_console_result(std::vector<bbox_t> const result_vec, std::vector<std::string> const obj_names)
+void show_console_result(std::vector<boundingbox> const result_vec, std::vector<std::string> const obj_names)
 {
 	for (auto &i : result_vec)
 	{
@@ -23,7 +24,7 @@ void show_console_result(std::vector<bbox_t> const result_vec, std::vector<std::
 	}
 }
 
-void draw_boxes(cv::Mat mat_img, std::vector<bbox_t> result_vec, std::vector<std::string> obj_names, int current_det_fps = -1, int current_cap_fps = -1)
+void draw_boxes(cv::Mat mat_img, std::vector<boundingbox> result_vec, std::vector<std::string> obj_names, int current_det_fps = -1, int current_cap_fps = -1)
 {
 	int const colors[6][3] = { {1,0,1}, {0,0,1}, {0,1,1}, {0,1,0}, {1,1,0}, {1,0,0} };
 
@@ -56,49 +57,40 @@ void draw_boxes(cv::Mat mat_img, std::vector<bbox_t> result_vec, std::vector<std
 
 int main(int argc, char *argv[])
 {
-	std::string name_file = "data/chaoyingDataset2.names";
-	std::string cfg_file = "data/yolov3-tiny-chaoyingDataset2.cfg";
-	std::string weights_file = "data/yolov3-tiny-chaoyingDataset2_46000.weights";
-	std::string file_name;
+	std::string name_file1 = "data/chaoyingDataset1.names";
+	std::string cfg_file1 = "data/yolov3-chaoyingDataset1.cfg";
+	std::string weights_file1 = "data/yolov3-chaoyingDataset1_50600.weights";
+	std::string file_name1;
 
 	float const thresh = 0.2;
 
-	Detector detector(cfg_file, weights_file);
+	//Detector detector(cfg_file, weights_file);
 	//initDetector(cfg_file, weights_file);
-	auto objNames = objects_names_from_file(name_file);
+	Detection* detector1 = AlgoDetectionManager::create(cfg_file1, weights_file1);
+	auto objNames1 = objects_names_from_file(name_file1);
 
-	//char filename[100] = "text.txt";
-	//char *ext = strrchr(filename, '.');
-	//if (ext)
-	//{
-	//	*ext = '\0';
-	//	ext++;
-	//}
-	//printf("name=%s\n", filename);
-	//printf("ext-name=%s\n", ext);
 
 	while (true)
 	{
-		std::cout << "input image filename: ";
-		if (file_name.size() == 0)
+		std::cout << "input image filename1: ";
+		if (file_name1.size() == 0)
 		{
-			std::cin >> file_name;
+			std::cin >> file_name1;
 		}
-		//if (file_name == "q")
-		//{
-		//	releaseDetector();
-		//	system("pause");
-		//	break;
-		//}
+		if (file_name1 == "q")
+		{
+			break;
+		}
 
-		std::string const fileExt = file_name.substr(file_name.find_last_of(".") + 1);
+		std::string const fileExt = file_name1.substr(file_name1.find_last_of(".") + 1);
 		if (fileExt == "jpg" || fileExt == "JPEG" || fileExt == "png")
 		{
-			cv::Mat matImg = cv::imread(file_name);
+			cv::Mat matImg = cv::imread(file_name1);
 
 			auto start = std::chrono::steady_clock::now();
 			//std::vector<boundingbox> resultVec = detectDL(matImg);
-			std::vector<bbox_t> resultVec = detector.detect(matImg);
+			std::vector<boundingbox> resultVec;
+			int err = detector1->detect(matImg, resultVec);
 			auto end = std::chrono::steady_clock::now();
 			std::chrono::duration<double> time = end - start;
 			std::cout << "Time: " << time.count() << " sec" << std::endl;
@@ -111,20 +103,84 @@ int main(int argc, char *argv[])
 					<< " " << resultVec[i].w << " " << resultVec[i].h
 					<<" score: "<< resultVec[i].prob<<std::endl;
 			}
-			draw_boxes(matImg, resultVec, objNames);
-			std::string outName = file_name.substr(0, file_name.rfind("."));
+			draw_boxes(matImg, resultVec, objNames1);
+			std::string outName = file_name1.substr(0, file_name1.rfind("."));
 			outName = outName + "_prediction.png";
 			//std::cout << outName << std::endl;
 			cv::imwrite(outName, matImg);
 			cv::namedWindow("detection", cv::WINDOW_NORMAL);
 			cv::imshow("detection", matImg);
-			show_console_result(resultVec, objNames);
+			show_console_result(resultVec, objNames1);
 			cv::waitKey(0);
 		} 
 		else
 		{
 			std::cout << " Finish soon!!!" << std::endl;
 		}
-		file_name.clear();
+		file_name1.clear();
 	}
+
+	AlgoDetectionManager::destroy(detector1);
+
+	std::string name_file2 = "data/chaoyingDataset2.names";
+	std::string cfg_file2 = "data/yolov3-tiny-chaoyingDataset2.cfg";
+	std::string weights_file2 = "data/yolov3-tiny-chaoyingDataset2_46000.weights";
+	std::string file_name2;
+
+	//Detector detector(cfg_file, weights_file);
+	//initDetector(cfg_file, weights_file);
+	Detection* detector2 = AlgoDetectionManager::create(cfg_file2, weights_file2);
+	auto objNames2 = objects_names_from_file(name_file2);
+
+	while (true)
+	{
+		std::cout << "input image filename2: ";
+		if (file_name2.size() == 0)
+		{
+			std::cin >> file_name2;
+		}
+		if (file_name2 == "q")
+		{
+			break;
+		}
+
+		std::string const fileExt = file_name2.substr(file_name2.find_last_of(".") + 1);
+		if (fileExt == "jpg" || fileExt == "JPEG" || fileExt == "png")
+		{
+			cv::Mat matImg = cv::imread(file_name2);
+
+			auto start = std::chrono::steady_clock::now();
+			//std::vector<boundingbox> resultVec = detectDL(matImg);
+			std::vector<boundingbox> resultVec;
+			int err = detector2->detect(matImg, resultVec);
+			auto end = std::chrono::steady_clock::now();
+			std::chrono::duration<double> time = end - start;
+			std::cout << "Time: " << time.count() << " sec" << std::endl;
+
+			for (size_t i = 0; i < resultVec.size(); ++i)
+			{
+				std::cout << "object " << i << std::endl;
+				std::cout << "obj id: " << resultVec[i].obj_id << " bbox: "
+					<< resultVec[i].x << " " << resultVec[i].y
+					<< " " << resultVec[i].w << " " << resultVec[i].h
+					<< " score: " << resultVec[i].prob << std::endl;
+			}
+			draw_boxes(matImg, resultVec, objNames2);
+			std::string outName = file_name2.substr(0, file_name2.rfind("."));
+			outName = outName + "_prediction.png";
+			//std::cout << outName << std::endl;
+			cv::imwrite(outName, matImg);
+			cv::namedWindow("detection", cv::WINDOW_NORMAL);
+			cv::imshow("detection", matImg);
+			show_console_result(resultVec, objNames2);
+			cv::waitKey(0);
+		}
+		else
+		{
+			std::cout << " Finish soon!!!" << std::endl;
+		}
+		file_name2.clear();
+	}
+
+	AlgoDetectionManager::destroy(detector2);
 }
--- src/AlgoDetection.cpp
+++ src/AlgoDetection.cpp
@@ -1,39 +1,73 @@
+#include <iostream>
+
 #include "AlgoDetection.h"
-#include "detection_class.hpp"
+#include "common.h"
+//#include "detection_class.hpp"
 
-Detector *detector;
+//Detector *detector;
+//
+//int initDetector(std::string cfg_filename, std::string weight_filename)
+//{
+//	if (cfg_filename.empty() || weight_filename.empty())
+//	{
+//		std::cout<<"cfg filename or weight filename is empty"<<std::endl;
+//		return -1;
+//	}
+//	detector = new Detector(cfg_filename, weight_filename);
+//	return 0;
+//}
+//
+//DECTECTIONDLL_API void releaseDetector()
+//{
+//	if (detector != NULL)
+//	{
+//		delete detector;
+//		detector = NULL;
+//		std::cout << "object detector released!" << std::endl;
+//	}
+//}
+//
+//DECTECTIONDLL_API std::vector<boundingbox> detectDL(cv::Mat image, float thresh/* = 0.2*/, bool use_mean/* = false*/)
+//{
+//	std::vector<boundingbox> detectionDL;
+//	if (image.data == NULL)
+//	{
+//		throw std::runtime_error("image is empty");
+//		return detectionDL;
+//	}
+//
+//	std::vector<bbox_t> detection = detector->detect(image);	
+//	for (size_t i = 0; i < detection.size(); ++i)
+//	{
+//		boundingbox bbox;
+//		bbox.x = detection[i].x;
+//		bbox.y = detection[i].y;
+//		bbox.w = detection[i].w;
+//		bbox.h = detection[i].h;
+//		bbox.prob = detection[i].prob;
+//		bbox.obj_id = detection[i].obj_id;
+//		detectionDL.push_back(bbox);
+//	}
+//	return detectionDL;
+//
+//}
 
-int initDetector(std::string cfg_filename, std::string weight_filename)
+AlgoDetection::AlgoDetection(std::string cfgFilename, std::string weightFilename, int gpu_id/* = 0*/)
+	:detector(cfgFilename, weightFilename, gpu_id)
 {
-	if (cfg_filename.empty() || weight_filename.empty())
-	{
-		std::cout<<"cfg filename or weight filename is empty"<<std::endl;
-		return -1;
-	}
-	detector = new Detector(cfg_filename, weight_filename);
-	return 0;
+
 }
 
-DECTECTIONDLL_API void releaseDetector()
+int AlgoDetection::detect(cv::Mat image, std::vector<boundingbox>& detections, float thresh /*= 0.2*/, bool use_mean /*= false*/)
 {
-	if (detector != NULL)
-	{
-		delete detector;
-		detector = NULL;
-		std::cout << "object detector released!" << std::endl;
-	}
-}
-
-DECTECTIONDLL_API std::vector<boundingbox> detectDL(cv::Mat image, float thresh/* = 0.2*/, bool use_mean/* = false*/)
-{
-	std::vector<boundingbox> detectionDL;
+	detections.clear();
 	if (image.data == NULL)
 	{
-		throw std::runtime_error("image is empty");
-		return detectionDL;
+		std::cout << "ERROR: Image is empty..." << std::endl;;
+		return CYAL_DETECTION_IMAGE_EMPTY_ERROR;
 	}
 
-	std::vector<bbox_t> detection = detector->detect(image);	
+	std::vector<bbox_t> detection = detector.detect(image);
 	for (size_t i = 0; i < detection.size(); ++i)
 	{
 		boundingbox bbox;
@@ -43,8 +77,7 @@ DECTECTIONDLL_API std::vector<boundingbox> detectDL(cv::Mat image, float thresh/
 		bbox.h = detection[i].h;
 		bbox.prob = detection[i].prob;
 		bbox.obj_id = detection[i].obj_id;
-		detectionDL.push_back(bbox);
+		detections.push_back(bbox);
 	}
-	return detectionDL;
-
+	return CYAL_DETECTION_SUCCESS;
 }
